6ce9a91f92df23e539c36cfde9673af5a189bb4d
aryadev127 <aryadewanggapuja@gmail.com>
2023-05-16 13:15:30 +0700

    cinema movie demo version

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,13 @@
 // mengambil data dari OMDb api
-function getData(title, callback) {
-  fetch(`http://www.omdbapi.com/?s="${title}"&apikey=9916612f`)
+async function getData(title, callback) {
+  await fetch(`https://www.omdbapi.com/?s="${title}"&apikey=9916612f`)
     .then((response) => response.json())
     .then((data) => callback(data));
 }
 
 async function detailMovie(id, callback) {
   const response = await fetch(
-    `http://www.omdbapi.com/?i=${id}&plot=full&apikey=9916612f`
+    `https://www.omdbapi.com/?i=${id}&plot=full&apikey=9916612f`
   )
     .then((response) => response.json())
     .then((data) => callback(data));
